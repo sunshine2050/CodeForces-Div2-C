18c1567481340e5075a702e7d7b79b48f74ee457
sunshine2050 <sunshin_samy@hotmail.com>
2016-12-08 04:30:29 +0200

    sum of digits %3

--- C. Lucky Tickets.cpp
+++ C. Lucky Tickets.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <map>
 using namespace std;
-map <int,int> m;
+int m[3];
 int main() {
 	int n;
 	cin>>n;
@@ -11,28 +11,8 @@ int main() {
 	for(int i=0;i<n;i++){
 		int x;
 		cin>>x;
-		int sum=0;
-		while(x){
-			sum+=x%10;
-			x/=10;
-		}
-		m[sum]++;
-		a.push_back(sum);
+		m[x%3]++;
 	}
-	int c=0;
-	for(int i=0;i<n;i++){
-		if(m[a[i]]<1) continue;
-		for(int j=1;j<=63;j++){
-			if(!m[a[i]]) break;
-			if((a[i]+j)%3==0 && m[j]){
-				//cout<<a[i]<<":"<<j<<":"<<m[a[i]]<<":"<<m[j]<<endl;
-				if(a[i]==j && m[j]<2) continue;
-				m[a[i]]--;
-				m[j]--;
-				c++;
-			}
-		}
-	}
-	cout<<c<<endl;
+	cout<<m[0]/2+min(m[1],m[2])<<endl;
 	return 0;
 }
